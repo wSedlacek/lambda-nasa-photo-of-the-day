ec1ce5c977d90a2217edb6d8d9be45e95efac36e
William Sedlacek <wsedlacekc@gmail.com>
2019-09-11 14:22:28 -0500

    Setup TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.scss';
+import React from 'react';
+
+import { NASAImgs } from './components/nasa-imgs/nasa-imgs.component';
+
+export const App = () => {
+  return <NASAImgs />;
+};
---
+++ src/components/card/card.component.tsx
@@ -0,0 +1,6 @@
+import './card.component.scss';
+import React from 'react';
+
+export const Card = () => {
+  return <div className='Card'>test</div>;
+};
