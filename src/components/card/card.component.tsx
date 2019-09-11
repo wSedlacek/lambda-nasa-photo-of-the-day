9c64ee555cd362fcd5f87d4e1e1df6171972a70e
William Sedlacek <wsedlacekc@gmail.com>
2019-09-11 16:03:53 -0500

    Small cleanup

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -5,7 +5,7 @@ import { NASAImg } from '../../models/NASAImg';
 export const Card = ({ data }: { data: NASAImg }) => {
   return (
     <div className='Card'>
-      <img src={data.url}></img>
+      <img src={data.url.includes('apod.nasa.gov/apod/') ? data.url : 'missing.jpg'}></img>
       <h2>{data.title}</h2>
       <p>{data.date}</p>
     </div>
