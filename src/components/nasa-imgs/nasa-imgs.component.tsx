b3171d20f449affe88e14b657aa63c1055729b95
William Sedlacek <wsedlacekc@gmail.com>
2019-09-11 16:04:01 -0500

    Get all images in the past week

--- src/components/nasa-imgs/nasa-imgs.component.tsx
+++ src/components/nasa-imgs/nasa-imgs.component.tsx
@@ -11,11 +11,18 @@ import { Card } from '../card/card.component';
 export const NASAImgs = () => {
   const [imgs, setImgs] = useState<NASAImg[]>([]);
   useEffect(() => {
-    axios
-      .get<NASAImg>(
-        'https://api.nasa.gov/planetary/apod?api_key=mmcs6j3Ms0Pt5cGtt4zaziKitBbMRtdLhl97JTJv'
-      )
-      .then(res => setImgs(imgs => [...imgs, res.data]));
+    for (let i = 0; i < 7; i++) {
+      const day = new Date();
+      day.setDate(day.getDate() - i);
+
+      axios
+        .get<NASAImg>(
+          `https://api.nasa.gov/planetary/apod?api_key=${
+            process.env.REACT_APP_NASA_KEY
+          }&date=${day.getFullYear()}-${day.getMonth() + 1}-${day.getDate()}`
+        )
+        .then(res => setImgs(imgs => [...imgs, res.data]));
+    }
   }, []);
 
   if (imgs.length === 0) return <h3 className='loading'>Loading...</h3>;
