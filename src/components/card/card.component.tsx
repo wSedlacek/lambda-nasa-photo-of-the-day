54ead80309b9efa3497a97143698a6c1e280881c
William Sedlacek <wsedlacekc@gmail.com>
2019-09-11 14:52:05 -0500

    Setup basic API fetch

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,6 +1,6 @@
 import './card.component.scss';
 import React from 'react';
 
-export const Card = () => {
-  return <div className='Card'>test</div>;
+export const Card = ({ img }: { img: string }) => {
+  return <div className='Card'>{img}</div>;
 };
---
+++ src/components/nasa-imgs/nasa-imgs.component.tsx
@@ -0,0 +1,28 @@
+import './nasa-imgs.component.scss';
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+import * as uuid from 'uuid';
+
+import { NASAImg } from '../../models/NASAImg';
+import { Card } from '../card/card.component';
+
+export const NASAImgs = () => {
+  const [imgs, setImgs] = useState<NASAImg[]>([]);
+  useEffect(() => {
+    axios
+      .get<NASAImg>(
+        'https://api.nasa.gov/planetary/apod?api_key=mmcs6j3Ms0Pt5cGtt4zaziKitBbMRtdLhl97JTJv'
+      )
+      .then(res => setImgs(imgs => [...imgs, res.data]));
+  }, []);
+
+  return (
+    <div>
+      {imgs.map(img => (
+        <Card img={img.url} key={uuid.v1()} />
+      ))}
+    </div>
+  );
+};
